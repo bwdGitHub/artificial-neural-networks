388a7266545d1a6a03a7fadcbe5f2e845987e427
Ben Wells-Day <benwellsday@gmail.com>
2018-12-26 14:12:26 +0000

    Made base class abstract, removed tests on base class, added documentation comments.

--- ann.py
+++ ann.py
@@ -1,14 +1,18 @@
-class ann:
+from abc import ABC, abstractmethod
+
+class ann(ABC):
+	## ann - Artifical Neural Network
+	# Parent class for various implementations		
 	
-	def __init__(this, layers = [], name = ''):	
-		this.name = name
-		this.layers = layers		
-		
+	@abstractmethod
 	def __str__(this):
-		return this.__class__.__name__ + " class with name " + this.name
-		
+		# Child classes should implement their display.
+		pass
 		
 class ann_by_layers(ann):
+	
+	## ann_by_layers - Artificial Neural Network constructed from a layer array.
+	# The assumption here is the network is sequential, input data x flows from layers[n] to layers[n+1].
 	
 	def __init__(this, layers = [], name = ''):	
 		this.name = name
--- tests.py
+++ tests.py
@@ -3,39 +3,8 @@ from ann import ann, ann_by_layers
 import io
 import contextlib
 
-class test_ann(unittest.TestCase):
-
-	def __init__(this, *args, **kwargs):
-		# Initialize test attributes and parameters
-		
-		# Parameter for valid inputs to cosntructor.
-		this.validInputs = [([],''), ([],'test'), ([], 'test')]
-		
-		# Super constructor.
-		super(test_ann, this).__init__(*args, **kwargs)
-
-	def test_constructor(this):
-	
-		# Test the constructor
-		
-		for layers, name in this.validInputs:
-			with this.subTest():
-				nn = ann(layers,name)
-				this.assertIsInstance(nn, ann)
-				this.assertEqual(nn.name, name)
-				this.assertEqual(nn.layers, layers)
-
-	def test_print(this):
-		
-		# Test the display of print(ann)
-		
-		for layers, name in this.validInputs:
-			with this.subTest():
-				nn = ann(layers,name)
-				catch_stdout = io.StringIO()
-				with contextlib.redirect_stdout(catch_stdout):
-					print(nn)
-				this.assertEqual(catch_stdout.getvalue(), nn.__class__.__name__ + " class with name " + nn.name + '\n') 
+# To Consider:
+# 1. Mock test the base class
 				
 class test_ann_by_layers(unittest.TestCase):
 
